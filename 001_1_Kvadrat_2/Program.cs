44fd46dff39ccffbb10b8dfb101f1fee1d1821e7
Natalya Molchanova <molchanova.nathalie@yandex.ru>
2022-10-03 21:09:35 +0300

    add 001_1_Kvadrat_2

---
+++ 001_1_Kvadrat_2/Program.cs
@@ -0,0 +1,10 @@
+﻿int numberA = 5;
+int numberB = 9;
+if(numberB == numberA * numberA)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число НЕ является квадратом второго");
+}
