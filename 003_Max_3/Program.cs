15dc5d9fe621b136e1209ffbc53094a27e6928c5
Natalya Molchanova <molchanova.nathalie@yandex.ru>
2022-10-03 22:07:32 +0300

    add 003_Max_3

---
+++ 003_Max_3/Program.cs
@@ -0,0 +1,13 @@
+﻿int numberA = 9;
+int numberB = 10;
+int numberC = 255;
+int max = numberA;
+if (max < numberB)
+{
+    Console.Write(max = numberB);
+}
+else if (max < numberC)
+{
+    Console.Write(max = numberC);
+}
+Console.Write(max);
