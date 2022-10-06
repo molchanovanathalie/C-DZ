9ef7845e9305d0ec4593a4bdef4ac3d4ecac6bbc
Natalya Molchanova <molchanova.nathalie@yandex.ru>
2022-10-06 20:42:21 +0300

    007

--- 007_vtoraya_3/Program.cs
+++ 007_vtoraya_3/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿int Number = new Random().Next(100, 999);
+int a = Number / 10;
+int b = a % 10;
+
+Console.WriteLine("Заданное число: " + Number);
+Console.WriteLine("Вторая цифра числа: " + b);
+
