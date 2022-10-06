0393dd6f8bb4c5acc768694ff3dcf23da6356ad9
Natalya Molchanova <molchanova.nathalie@yandex.ru>
2022-10-06 20:47:07 +0300

    008

--- 008_10_99/Program.cs
+++ 008_10_99/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿int Number = new Random().Next(10, 99);
+int a = Number / 10;
+int b = Number % 10;
+
+Console.WriteLine("Число из отрезка: " + Number);
+if (a > b)
+{
+    Console.WriteLine("Наибольшая цифра: " + a);
+}
+else
+{
+    Console.WriteLine("Наибольшая цифра: " + b);
+}
+if (a == b)
+{
+    Console.WriteLine("Цифры равны");
+}
